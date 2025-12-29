14ab9baa5b6bff455688b51e3e09a92a6a735f26
chucnorrisfully <benjamin.moeckl@venture-ship.com>
2025-12-29 19:59:43 +0100

    implemented login and register

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
+	"encoding/json"
 	"fmt"
+	"math/rand"
+	"net/http"
 	"slices"
+	"strings"
 	"time"
 )
 
 // type repeating date
 type user struct {
-	id      int
-	friends []int
+	id       int
+	friends  []int
+	secret   string
+	token    string
+	tokenEnd time.Time
 }
 type schedule struct {
 	st    time.Time
@@ -40,7 +49,114 @@ func NewDB() *database {
 }
 
 func main() {
+	db := NewDB()
+	launchServer(db)
+}
 
+func launchServer(db *database) {
+	http.HandleFunc("/login", db.authHandler)
+	http.HandleFunc("/register", db.registerHandler)
+	http.HandleFunc("/addFriend", db.addFriendHandler)
+
+	http.ListenAndServe("localhost:8080", http.DefaultServeMux)
+}
+
+func (db *database) addFriendHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (db *database) IsLoggedIn(r *http.Request) int {
+	reqToken := r.Header.Get("Authorization")
+	reqToken = strings.TrimPrefix(reqToken, "Bearer ")
+
+	uid := -1
+	for _, u := range db.users {
+		if u.token == reqToken && u.tokenEnd.After(time.Now()) {
+			return u.id
+		}
+	}
+	return uid
+}
+
+func (db *database) authHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	pw := fmt.Sprintf("%v", time.Now())
+	uid := -1
+
+	decoder := json.NewDecoder(r.Body)
+	var creds struct {
+		Uid int
+		Pw  string
+	}
+	err := decoder.Decode(&creds)
+	if err != nil {
+		uid = creds.Uid
+		pw = creds.Pw
+	}
+	userPwHash := ""
+	u := &user{}
+	if uid != -1 {
+		if u2, ok := db.users[uid]; ok {
+			u = u2
+		}
+	}
+	userPwHash = u.secret
+
+	pwHash := sha256.Sum256([]byte(pw))
+
+	if subtle.ConstantTimeCompare(pwHash[:], []byte(userPwHash)[:]) == 1 && u.id == uid {
+		token := sha256.Sum256([]byte(fmt.Sprintf("%v%v", uid, time.Now())))
+		u.token = string(token[:])
+		u.tokenEnd = time.Now().Add(time.Hour * 4)
+
+		enc := json.NewEncoder(w)
+		enc.Encode(struct{ token string }{u.token})
+		w.WriteHeader(200)
+		fmt.Printf("logged in user %v", uid)
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+}
+
+func (db *database) registerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var newPW struct {
+		Pw string `json:"pw"`
+	}
+	err := decoder.Decode(&newPW)
+	if err == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	uid := -1
+	for {
+		uid = rand.Intn(1000)
+		u, err := db.addUser(uid)
+		if err == nil {
+			sec := sha256.Sum256([]byte(newPW.Pw))
+			u.secret = string(sec[:])
+			break
+		}
+	}
+
+	if uid != -1 {
+		fmt.Printf("added new user %v", uid)
+		wr := json.NewEncoder(w)
+		wr.Encode(struct{ Uid int }{Uid: uid})
+		w.WriteHeader(200)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
 func (db *database) findRoutes(startTime time.Time, startU *user, goalId int, timeout time.Duration, maxMeetings int) ([]route, error) {
